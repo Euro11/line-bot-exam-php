2efce05778b6954e377be3f4429b40128f9b2b5f
Euro11 <40200008+Euro11@users.noreply.github.com>
2019-03-04 15:30:00 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '/86mgHD+mZF2QIUZ9m3d/rNFrnJfTdZlFHmRDSeIwU56ZbH5d5x2hE6GDL/iR5Z1mJNurDeZcHQmLYjofqhCTzO4ktlEabRlNg7/KxRS9iumc538AZxYrAk9zO4fCh1snDjAtOkAcyNeZfjfMPiyAgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
